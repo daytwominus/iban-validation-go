d689a4a1c7e43b328d88eb6e085885c24074443e
daytwominus <panasiux@gmail.com>
2019-06-21 11:50:07 +0200

    batch processing, readme

---
+++ README.md
@@ -0,0 +1,71 @@
+# IBAN validation
+
+Code and infrastructure to validate IBAN written in Golang.
+
+## Building
+
+1. install Go
+
+## Web server
+
+1. Navigate to ./web dir, run server
+
+```go
+
+go run ./iban-web-server.go
+
+```
+
+2. Now one can perform calls:
+
+```cmd
+
+curl -i http://127.0.0.1:8080/iban/validate/MT31MALT0110000000000000000012
+
+```
+
+Response:
+
+```json
+
+{
+    "success": false,
+    "message": "IBAN string has incorrect length"
+}
+
+```
+
+Batch processing:
+
+```cmd
+
+curl --location --request POST "http://127.0.0.1:8080/iban/validate" --header "Content-Type: application/json"   --data "{\"ibans\":[\"MT31MALT01100000000000000000123\",\"GB82WEST1234569876543\"]}"
+
+```
+
+Response:
+
+```json
+
+[
+    {
+        "success": true,
+        "message": ""
+    },
+    {
+        "success": false,
+        "message": "IBAN string has incorrect length"
+    }
+]
+
+```
+
+## Testing
+
+Navigate to ./iban, run
+
+```go
+
+go test
+
+```
--- web/iban-web-server.go
+++ web/iban-web-server.go
@@ -1,12 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/ant0ine/go-json-rest/rest"
 	"github.com/daytwominus/iban-validation-web-go/iban"
 )
 
 func main() {
-	//fmt.Println(stringutil.Reverse("!oG ,olleH"))
-	fmt.Println(iban.Validate("GB82WEST12345698765432"))
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		rest.Get("/iban/validate/#iban", ValidateIban),
+		rest.Post("/iban/validate", ValidateMultiple),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+}
+
+func ValidateIban(w rest.ResponseWriter, req *rest.Request) {
+	res := iban.Validate(req.PathParam("iban"))
+	w.WriteJson(res)
+}
+
+type MultipleIbans struct {
+	Ibans []string
+}
+
+func ValidateMultiple(w rest.ResponseWriter, r *rest.Request) {
+	multiple := MultipleIbans{}
+	err := r.DecodeJsonPayload(&multiple)
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var res []iban.IbanValidationResult
+	for _, s := range multiple.Ibans {
+		res = append(res, iban.Validate(s))
+	}
+
+	w.WriteJson(res)
 }
